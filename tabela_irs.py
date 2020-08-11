a1e3bea37b507859525393cd500c14298a9ee82a
Joao Filipe Rocha <jfsllrocha@gmail.com>
2020-08-12 00:21:34 +0100

    adicionados AZO e MAD

--- tabela_irs.py
+++ tabela_irs.py
@@ -1,36 +1,70 @@
-import xlrd
+# import xlrd
+import pandas as pd
 from collections import OrderedDict
 import simplejson as json
+import codecs
 
-wb = xlrd.open_workbook('tabelas_ret_irs_2017.xlsx')
-sh = wb.sheet_by_index(0)
-tabela = []
-tab = OrderedDict()
-for rownum in range(0, sh.nrows):  
-   row_values = sh.row_values(rownum)
-   firstColVals = row_values[1].split(' ')
-   if len(firstColVals) == 11 and firstColVals[0] == 'TABELAS':      
-      #if firstColVals[8] not in tab:
-      #tab['local'] = firstColVals[8] 
-      localRowNum = rownum
-   if rownum - (localRowNum + 4) == 0:
-      escaloes = OrderedDict()
-      categoria = row_values[1]     
-   if row_values[1] == 'Até' or   row_values[1] == 'Superior a' :
-       dependentes = OrderedDict();       
-       dependentes["0"] = row_values[3]
-       dependentes["1"] = row_values[4]
-       dependentes["2"] = row_values[5]
-       dependentes["3"] = row_values[6]
-       dependentes["4"] = row_values[7]
-       dependentes["5"] = row_values[8]
-       escaloes[ row_values[2]]  = dependentes
-       tab[categoria] = escaloes
-    
-print(tab)
-tabela.append(tab)
 
-j= json.dumps(tabela)
+def setData(tx, ds, writeMode):
+    taxas_retencao = dict()
+    tabela = list()
+    tab = OrderedDict()
+    localRowNum = 0
+    firstColVals = ''
+    print("Extrair dados de ", tx)
+    for index, col in ds.iterrows():
+        #print(index, col[0])
+        #print(isinstance(col[0], str))
 
-with open('data.json', 'w') as f:
-   f.write(j)
+        if isinstance(col[0], str):
+            firstColVals = col[0].split(' ')
+            #print(len(firstColVals), firstColVals[0])
+            if firstColVals[0] == 'TABELAS':
+                localRowNum = index
+
+        #print(index - (localRowNum + 4))
+        if index - (localRowNum + 4) == 0:
+            escaloes = list()
+            categoria = col[0]
+        if col[0] == 'Até' or col[0] == 'Superior a':
+            dependentes = OrderedDict()
+            dependentes['max'] = col[1]
+            dependentes['0'] = round(col[2], 3)
+            dependentes['1'] = round(col[3], 3)
+            dependentes['2'] = round(col[4], 3)
+            dependentes['3'] = round(col[5], 3)
+            dependentes['4'] = round(col[6], 3)
+            dependentes['5'] = round(col[7], 3)
+            escaloes.append(dependentes)
+            tab[categoria] = escaloes
+
+    tabela.append(tab)
+    #print(tabela)
+    taxas_retencao[tx] = tabela
+
+    j = json.dumps(taxas_retencao)
+    #print(j)
+    print("Gravar dados de ", tx)
+    if writeMode == 'a':
+        j = "," + j
+    with open('data.json', writeMode) as f:
+        f.write(j)
+
+
+CNT = pd.read_excel(
+    'https://info.portaldasfinancas.gov.pt/pt/apoio_contribuinte/tabela_ret_doclib/Documents/Tabelas_Ret_IRS_2020_Continente.xlsx',
+    sheet_name=0)
+print("Tabela Continente carregada para CNT")
+setData("CNT", CNT, 'w')
+
+AZO = pd.read_excel(
+    'https://info.portaldasfinancas.gov.pt/pt/apoio_contribuinte/tabela_ret_doclib/Documents/Tabelas_Ret_IRS_2020_Acores.xlsx',
+    sheet_name=0)
+print("Tabela Açores carregada para AZO")
+setData("AZO", AZO, 'a')
+
+MAD = pd.read_excel(
+    'https://info.portaldasfinancas.gov.pt/pt/apoio_contribuinte/tabela_ret_doclib/Documents/Tabelas_Ret_IRS_2020_RAMadeira.xlsx',
+    sheet_name=0)
+print("Tabela Madeira carregada para MAD")
+setData("MAD", MAD, 'a')
